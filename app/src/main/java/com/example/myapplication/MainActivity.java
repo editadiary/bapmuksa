8627bb3394729b65411d550e9d715484f3258664
editadiary <blessedita@kaist.ac.kr>
2022-06-30 13:24:42 +0900

    emulator operates

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,82 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+
+    String[] names = {"test image"};
+    int[] images = {R.drawable.ic_launcher_background};
+//
+//    String[] names = {"image1", "image2"};
+//    int[] images = {R.drawable.image1, R.drawable.image2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+
+        CustomAdapter customAdapter = new CustomAdapter(names, images, this);
+
+        gridView.setAdapter(customAdapter);
     }
+
+    public class CustomAdapter extends BaseAdapter{
+
+        private String[] imageNames;
+        private int[] imagesPhoto;
+        private Context context;
+        private LayoutInflater layoutInflater;
+
+        public CustomAdapter(String[] imageNames, int[] imagesPhoto, Context context) {
+            this.imageNames = imageNames;
+            this.imagesPhoto = imagesPhoto;
+            this.context = context;
+            this.layoutInflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return imagesPhoto.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            if(view == null){
+                view = layoutInflater.inflate(R.layout.row_items, viewGroup, false);
+            }
+
+            TextView tvName = view.findViewById(R.id.tvName);
+            ImageView imageView = view.findViewById(R.id.imageView);
+
+            tvName.setText(imageNames[i]);
+            imageView.setImageResource(imagesPhoto[i]);
+
+            return view;
+        }
+    }
+
 }
