909d4e40edcb5f80c970dc23276b0ee92f0e2100
이성진 <52152015+mobius29@users.noreply.github.com>
2022-07-04 21:37:21 +0900

    edit spinner

--- app/src/main/java/com/example/myapplication/Recommend/RecommendFirstActivity.java
+++ app/src/main/java/com/example/myapplication/Recommend/RecommendFirstActivity.java
@@ -2,64 +2,82 @@ package com.example.myapplication.Recommend;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication.Contact.ContactDetailActivity;
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+
 public class RecommendFirstActivity extends AppCompatActivity implements View.OnClickListener {
-    // private final String[] string_id = {"spinnerTag", "minimumDate", "maximumDate", "searchBtn"};
-    private Spinner tagSpinner;
-    private EditText minimumET, maximumET;
-    private Button searchBtn;
-    private int tagIdx = 0;
+    private ArrayList<String> foodTags;
+    private Spinner foodSpinner;
+    private FoodSpinnerAdapter foodAdapter;
+    private String selectedFood;
+    Button searchBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_firstpage);
 
-        tagSpinner = findViewById(R.id.spinnerTag);
-        minimumET = findViewById(R.id.minimumDate);
-        maximumET = findViewById(R.id.maximumDate);
+        initVariables();
+        initTags();
+        initViews();
+        initListener();
+    }
+
+    private void initVariables() {
+        foodTags = new ArrayList<>();
+    }
+
+    private void initTags() {
+        final String[] foods = {"전체", "한식", "중식", "일식", "양식", "후식", "기타"};
+        int sz = foods.length;
+
+        for(int i = 0; i < sz; ++i) {
+            foodTags.add(foods[i]);
+        }
+    }
+
+    private void initViews() {
+        foodAdapter = new FoodSpinnerAdapter(this, foodTags);
+
+        foodSpinner = findViewById(R.id.foodSpinner);
+        foodSpinner.setAdapter(foodAdapter);
+
         searchBtn = findViewById(R.id.searchBtn);
+    }
+
+    private void initListener() {
+        foodSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedFood = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         searchBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        int min = -1, max = -1;
-        String tag = tagSpinner.getSelectedItem().toString();
-        if(minimumET.getText() != null) min = Integer.parseInt(minimumET.getText().toString());
-        if(maximumET.getText() != null) max = Integer.parseInt(maximumET.getText().toString());
+        int min = 0, max = 100;
+        Intent intent = new Intent(this, RecommendSecondActivity.class);
+        intent.putExtra("min", min)
+                .putExtra("max", max)
+                .putExtra("selected", selectedFood);
 
-        if(min == -1 || max == -1) {
-            Toast.makeText(this, "기간을 입력해주세요", Toast.LENGTH_LONG).show();
-        }
-
-        else if(max < min) {
-            Toast.makeText(this, "기간을 제대로 입력해주세요", Toast.LENGTH_LONG).show();
-        }
-
-        else {
-            Intent intent = new Intent(getApplicationContext(), RecommendSecondActivity.class);
-            intent.putExtra("min", min)
-                    .putExtra("max", max)
-                    .putExtra("tag", tag);
-
-            startActivity(intent);
-        }
+        startActivity(intent);
     }
 }
