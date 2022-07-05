8699eb0d67cf400f8ec82238263ba5e0cce8ae93
editadiary <blessedita@kaist.ac.kr>
2022-07-05 20:08:19 +0900

    minor error fixed

--- app/src/main/java/com/example/myapplication/Gallery/ClickedItemEditActivity.java
+++ app/src/main/java/com/example/myapplication/Gallery/ClickedItemEditActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
